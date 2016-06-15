5c6ef4bb863940b24f4fb47b16a47d929282ac41
Danny Eck <daniel.eck@accenture.com>
2016-06-14 23:12:56 -0500

    It lives
    Ripped out the `worker.py` bits from the [fo_api](#link?) repo to be a
    standalone thing. (As it should be). Still doesn't do much more than
    sleep and then report that its done, but the messaging protocol is
    becoming more concrete. Time to check in so @Jon can replicate an
    environment.

---
+++ src/worker.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import json
+import pika
+import time
+from pymongo import MongoClient
+import gridfs
+from bson.objectid import ObjectId
+
+db = MongoClient()['app']
+fs = gridfs.GridFS(db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+channel.queue_declare(queue='notify_queue', durable=False)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    # consume
+    print(" [x] Received %r" % body)
+
+    msg = json.loads(body.decode('utf-8'))
+    # job = db.jobs.find_one({'_id': ObjectId(body.decode('utf-8'))})
+    job = db.jobs.find_one({'_id': ObjectId(msg['_id'])})
+
+    # set status to working
+    db.jobs.update_one(
+        # {'_id': ObjectId(body.decode('utf-8'))},
+        {'_id': job['_id']},
+        {
+            "$set": {
+                "status": "working"
+            }
+        }
+    )
+
+    # retrieve context
+    # job
+
+    # retrieve artifacts
+
+
+    # do work
+    time.sleep(5)
+
+    # set status to done
+    db.jobs.update_one(
+        {'_id': job['_id']},
+        {
+            "$set": {
+                "status": "done"
+            }
+        }
+    )
+
+    res = dict(
+        job_id=msg['_id'],
+        user_id=msg['userId'],
+        outcome='Success'
+    )
+
+    # send notification
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    channel.basic_publish(exchange='',
+                          routing_key='notify_queue',
+                          # body='Job: 123 requested by userId: 456 is done!',
+                          body=json.dumps(res),
+                          properties=pika.BasicProperties(
+                              # delivery_mode=2,  # make message persistent
+                          ))
+
+    print(" [x] Done")
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback,
+                      queue='task_queue')
+
+channel.start_consuming()
