305ce9fdec6fa504c175b18e6be308a3a702e36e
tkmabjs <jonathan.condon@kohls.com>
2016-08-26 16:24:51 -0500

    Status running.

--- src/worker.py
+++ src/worker.py
@@ -59,19 +59,14 @@ def main():
         # current_user = job['userId']
         # job_status = job['status']
 
-        '''
-        Fetch (twice) and write once to the document by updating status 
-        '''
-        def status_update(status):
-            # TODO: can find_one_and_update return json? minimize read/write to DB
-            db.jobs.find_one_and_update(
-                {'_id': job_id},
-                {
-                    "$set": {
-                        "status": status
-                    }
+        db.jobs.update_one(
+            {'_id': job['_id']},
+            {
+                "$set": {
+                    "status": "Running"
                 }
-            )
+            }
+        )
 
                 
         # Hardik Code to parse out information
@@ -83,74 +78,7 @@ def main():
             # file=file.dropna()
             return file
 
-        # if isPreop:
-        #     status_update('preoptimizing')
-        #     '''
-        #     on failure we must make sure we log the fail and send a msg back to mq
-        #     '''
-             
-        #     # fixtureArtifact=fetch_artifact(msg["artifacts"]["spaceArtifactId"]).set_index("Store")
-        #     # transactionArtifact=fetch_artifact(msg["artifacts"]["salesArtifactId"]).set_index("Store")
-        #     # opt_amt = preoptimize(fixtureArtifact,transactionArtifact,float(msg["metricAdjustment"]),float(msg["salesPenetrationThreshold"]),msg["optimizedMetrics"],msg["increment"])
-        #     # create_new_res_artifact('spaceResId',my_test_file,'spaceArtifact')
-        #     job_status = status_update('optimizing')
-        #     isOptimize = True
-        #     # raise Exception('FAIL!')
-        #     '''
-        #     Write res to database for next step in optimize to use...
-        #     '''
-        # if isOptimize:
-        #     if not isPreop and isOptimize:
-        #         print('Rabbit must have failed, but no worries we will start here!')
-        #         # pull artifact from gridfs so optimzation can use it    
-        #     else:
-        #         print('Rabbit must be working smoothly for once...')
-        #         # create_new_res_artifact('salesResId',my_test_file,'salesArtifact')
-        #         # use what we have in the one worker life span
-        #     # RUN OPTIMIZATION PIECE...
         
-        # # TODO: (FIX) for some stupid reason create_new_res_artifact is overwritting on each time it runs...
-        # # create_new_res_artifact('masterOpResId',my_test_file,'optimizedArtifact')
-        # status_update('done')
-        
-        # TODO: turn all these if statements into a consolodated func
-        # if job_status == 'pending' or job_status == 'preoptimizing':
-        #     status_update('preoptimizing')
-        #     '''
-        #     on failure we must make sure we log the fail and send a msg back to mq
-        #     '''
-        #     # raise Exception('FAIL!') 
-        #     # fixtureArtifact=fetch_artifact(msg["artifacts"]["spaceArtifactId"]).set_index("Store")
-        #     # transactionArtifact=fetch_artifact(msg["artifacts"]["salesArtifactId"]).set_index("Store #")
-        #     # opt_amt = preoptimize(fixtureArtifact,transactionArtifact,float(msg["metricAdjustment"]),float(msg["salesPenetrationThreshold"]),msg["optimizedMetrics"],msg["increment"])
-        #     create_new_res_artifact('masterSpaceResId',my_test_file,'salesArtifact')
-        #     '''
-        #     Write res to database for next step in optimize to use...
-        #     '''
-        
-        # job_status = status_update('optimizing') 
-        
-        # if job_status == 'optimizing':
-        #     '''
-        #     on failure we must make sure we log the fail and send a msg back to mq
-        #     we also need to pull from mongo the document so we can continue optimzation
-        #     DON'T PULL FROM MONGO UNLESS IT DIES AND PICKS UP FROM WHERE WE LEFT OFF.
-        #     '''
-        #     print('For the sake of python...')
-        #     # raise Exception('FAIL!')
-        #     # optimize(opt_amt,msg["tierCounts"],msg["spaceBounds"],msg["increment"])
-        #     '''
-        #     Write res to database for next step in optimize to use...
-        #     '''
-
-        # status_update('done')
-        #What are we passing through the optimize params? is there anything?
-        #Probably need to call the preoptimize function right here...
-        #Then call optimize? or does optimize from preop call optimize...
-        # if isinstance(msg["artifacts"]["futureSpaceId"],str):
-        #     futureSpace=fetch_artifact(msg["artifacts"]["futureSpaceId"]).set_index("Store")
-        # if isinstance(msg["artifacts"]["futureSpaceId"],str):
-            # brandExitArtifact=brandExitMung(fetch_artifact(msg["artifacts"]["brandExitArtifactId"]))
         fixtureArtifact=fetch_artifact(msg["artifacts"]["spaceArtifactId"])
         transactionArtifact=fetch_artifact(msg["artifacts"]["salesArtifactId"])
         transactionArtifact=transactionArtifact.drop(transactionArtifact.index[[0]]).set_index("Store")
