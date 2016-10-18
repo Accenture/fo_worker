d268d10096cd7344242ad28e56977208a5d4c6ab
nick.gentile <nick.gentile@accenture.com>
2016-10-18 17:09:02 -0500

    Changing the number of threads to 8.

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -263,7 +263,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     # NewOptim.solve()
 
     # Solve the problem using open source solver
-    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,maxSeconds=43200,threads=4))
+    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,maxSeconds=43200,threads=8))
     # solver = "CBC" #for unit testing
 
     #Solve the problem using Gurobi
