3e96d88307ab73028eccc800554f28a32296363c
Sylvain, Kenneth L <kenneth.l.sylvain@accenture.com>
2016-10-19 17:09:17 -0500

    Capital R

--- src/FixtureOptimization/CurveFitting.py
+++ src/FixtureOptimization/CurveFitting.py
@@ -30,7 +30,7 @@ def curveFittingBS(big_master_data,bound_input,increment_size,PCT_Space_Change_L
 
     #Source the R code
     r_source = robjects.r['source']
-    r_source('src/FixtureOptimization/rCurveFitting.r')
+    r_source('src/FixtureOptimization/rCurveFitting.R')
 
     # # Extract the main function from the R code
     r_curvefitting_boundsetting = robjects.globalenv['curvefitting_boundsetting']
