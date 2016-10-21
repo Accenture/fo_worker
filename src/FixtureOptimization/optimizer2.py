f50ff2a0c870f9d999c2ed6f43e596518bf093aa
Sylvain, Kenneth L <kenneth.l.sylvain@accenture.com>
2016-10-21 16:21:49 -0500

    1 Second

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -266,7 +266,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     # mergedPreOptCF.to_csv('test.csv',index=False,sep=',')
 
     # Solve the problem using open source solver
-    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,maxSeconds=115200))
+    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,maxSeconds=1))
     # solver = "CBC" #for unit testing
 
     #Solve the problem using Gurobi
