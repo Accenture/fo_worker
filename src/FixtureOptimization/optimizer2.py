96d87db2489dfbbd29973a88e16b523196af15f4
tkmahbz <peter.alfors@kohls.com>
2016-10-25 21:40:43 -0500

    changed fracGap to .6

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -269,7 +269,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     # NewOptim.writeMPS(str(jobName)+".mps")
     # return
     # Solve the problem using open source solver
-    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,fracGap=.5))
+    NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,fracGap=.6))
     # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,cuts=100,options=['sec','600'],fracGap=.1))
     # solver = "CBC" #for unit testing
 
