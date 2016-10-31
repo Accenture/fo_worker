e7d2987aeb3686cc43393ca15016b7edc9afe418
tkmabjs <jonathan.condon@kohls.com>
2016-10-31 14:50:18 -0500

    Ken's changes

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -10,9 +10,10 @@ import pymongo as pm
 import gridfs
 import config
 import datetime as dt
+import traceback
 
 # Run tiered optimization algorithm
-def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights,cfbsOutput,preOpt,salesPen,threadCount=None,fractGap=None):
+def optimizeNoNames(methodology,jobName,Stores,Categories,tierCounts,increment,weights,cfbsOutput,preOpt,salesPen,threadCount=None,fractGap=None):
     print('in the new optimization')
     # Helper function for optimize function, to create eligible space levels
     # print(cfbsOutput.columns)
@@ -67,8 +68,28 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
             value = str_cat["Scaled_Alpha_" + variable] * math.erf(
                 (space - str_cat["Scaled_Shift_" + variable]) / (math.sqrt(2) * str_cat["Scaled_Beta_" + variable]))
 
-        return value
+        return round(value,2)
 
+    def forecast2(str_cat, space, variable,increment):
+        def roundValue(cVal, increment):
+            if np.mod(round(cVal, 3), increment) > increment / 2:
+                cVal = round(cVal, 3) + (increment - (np.mod(round(cVal, 3), increment)))
+            else:
+                cVal = round(cVal, 3) - np.mod(round(cVal, 3), increment)
+            return cVal
+
+        if space < str_cat["Scaled_BP_" + variable]:
+            value = space * (str_cat["Scaled_Alpha_" + variable] * (math.erf(
+                (str_cat["Scaled_BP_" + variable] - str_cat["Scaled_Shift_" + variable]) / ((
+                math.sqrt(2) * str_cat["Scaled_Beta_" + variable])))) / str_cat["Scaled_BP_" + variable])
+        else:
+            value = str_cat["Scaled_Alpha_" + variable] * math.erf(
+                (space - str_cat["Scaled_Shift_" + variable]) / (math.sqrt(2) * str_cat["Scaled_Beta_" + variable]))
+
+        value=roundValue(value,increment)
+
+        return value
+    
     # Helper function for optimize function, to create objective function of SPU by level for Enhanced optimizations
     def createNegSPUByLevel(Stores, Categories, Levels, curveFittingOutput, enhMetrics):
 
@@ -188,6 +209,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
         objectivetype = "Total Error"
     else: #since methodology == "enhanced"
         objective = createNegSPUByLevel(Stores, Categories, Levels, mergedPreOptCF, weights)
+        # objective2 = createNegSPUByLevel(Stores, Categories, Levels, mergedPreOptCF, weights,increment)
         objectivetype = "Total Negative SPU"
 
     # pd.DataFrame(objective).to_csv(str(jobName)+'objective.csv',sep=",")
@@ -203,9 +225,9 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
         # TODO: Exploratory analysis on impact of balance back on financials for Enhanced
         # Store-level balance back constraint: the total space allocated to products at each location must be within the individual location balance back tolerance limit
         NewOptim += lpSum([(st[Store][Category][Level]) * Level for (j, Category) in enumerate(Categories) for (k, Level) in
-                           enumerate(Levels)]) >= locSpaceToFill[Store] * (1 - locBalBackBoundAdj[Store]), "Location Balance Back Lower Limit: STR " + str(Store)
+                           enumerate(Levels)]) >= locSpaceToFill[Store] * (1 - locBalBackBoundAdj[Store])
         NewOptim += lpSum([(st[Store][Category][Level]) * Level for (j, Category) in enumerate(Categories) for (k, Level) in
-                           enumerate(Levels)]) <= locSpaceToFill[Store] * (1 + locBalBackBoundAdj[Store]), "Location Balance Back Upper Limit: STR " + str(Store)
+                           enumerate(Levels)]) <= locSpaceToFill[Store] * (1 + locBalBackBoundAdj[Store])
 
         # EXPLORATORY ONLY: ELASTIC BALANCE BACK
         # Penalize balance back by introducing an elastic subproblem constraint
@@ -218,11 +240,11 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
         for (j,Category) in enumerate(Categories):
             # print('we got through the first part')
             # Only one selected tier can be turned on for each product at each location.
-            NewOptim += lpSum([st[Store][Category][Level] for (k,Level) in enumerate(Levels)]) == 1, "One Tier per Location: STR " + str(Store) + ", CAT " + str(Category)
+            NewOptim += lpSum([st[Store][Category][Level] for (k,Level) in enumerate(Levels)]) == 1
 
             # The space allocated to each product at each location must be between the minimum and the maximum allowed for that product at the location.
-            NewOptim += lpSum([st[Store][Category][Level] * Level for (k,Level) in enumerate(Levels)] ) >= mergedPreOptCF["Lower_Limit"].loc[Store,Category],"Space Lower Limit: STR " + str(Store) + ", CAT " + str(Category)
-            NewOptim += lpSum([st[Store][Category][Level] * Level for (k,Level) in enumerate(Levels)] ) <= mergedPreOptCF["Upper_Limit"].loc[Store,Category],"Space Upper Limit: STR " + str(Store) + ", CAT " + str(Category)
+            NewOptim += lpSum([st[Store][Category][Level] * Level for (k,Level) in enumerate(Levels)] ) >= mergedPreOptCF["Lower_Limit"].loc[Store,Category]
+            NewOptim += lpSum([st[Store][Category][Level] * Level for (k,Level) in enumerate(Levels)] ) <= mergedPreOptCF["Upper_Limit"].loc[Store,Category]
             if mergedPreOptCF['Sales Penetration'].loc[Store,Category] < salesPen:
                 NewOptim += st[Store][Category][0] == 1
 
@@ -231,12 +253,12 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     for (j,Category) in enumerate(Categories):
         # totalTiers=totalTiers+tierCounts[Category][1]
         # The number of created tiers must be within the tier count limits for each product.
-        NewOptim += lpSum([ct[Category][Level] for (k,Level) in enumerate(Levels)]) >= tierCounts[Category][0], "Tier Count Lower Limit: CAT " + str(Category)
-        NewOptim += lpSum([ct[Category][Level] for (k,Level) in enumerate(Levels)]) <= tierCounts[Category][1], "Tier Count Upper Limit: CAT " + str(Category)
+        NewOptim += lpSum([ct[Category][Level] for (k,Level) in enumerate(Levels)]) >= tierCounts[Category][0]
+        NewOptim += lpSum([ct[Category][Level] for (k,Level) in enumerate(Levels)]) <= tierCounts[Category][1]
 
         for (k,Level) in enumerate(Levels):
             # A selected tier can be turned on if and only if the created tier at that level for that product is turned on.
-            NewOptim += lpSum([st[Store][Category][Level] for (i,Store) in enumerate(Stores)])/len(Stores) <= ct[Category][Level], "Selected-Created Tier Relationship: CAT " + str(Category) + ", LEV: " + str(Level)
+            NewOptim += lpSum([st[Store][Category][Level] for (i,Store) in enumerate(Stores)])/len(Stores) <= ct[Category][Level]
 
             # EXPLORATORY ONLY: MINIMUM STORES PER TIER
             # Increases optimization run time
@@ -261,12 +283,12 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
 
     #Time stamp for optimization solve time
     # start_seconds = dt.datetime.today().hour*60*60+ dt.datetime.today().minute*60 + dt.datetime.today().second
-    print("to the solver we go")
     # NewOptim.solve()
 
     mergedPreOptCF.reset_index(inplace=True)
     # mergedPreOptCF.to_csv(str(jobName)+'.csv',sep=',')
-    NewOptim.writeMPS(str(jobName)+".mps")
+    # NewOptim.writeMPS(str(jobName)+".mps")
+    # return
 
     # if jobName[0:4] == 'flag':
     #     for char in jobName[4::]:
@@ -274,107 +296,66 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     #             fractGap.append(char)
     #     fractGap=int(jobName[4:6])
     # Solve the problem using open source solver
+    print('optional hidden parameters')
+    
     if threadCount == None:
         threadCount = 4
-    if fractGap == None:
-        fractGap = 0
-    if jobName[0:4] == 'deci':
-        fractGap=float(jobName[4:6])/100
-    if jobName[0:4] == 'flag':
-        fractGap=float(jobName[4:6])
     if 'PreSolve' in jobName:
         preSolving = True
     else:
         preSolving = False
+    print('none parameters')
+    def searchParam(string,search):
+        if search in something:
+            begin=something.find(search)
+            length=0
+            for char in something[(len(search)+begin)::]:
+                try:
+                    int(char)
+                    length=length+1
+                except:
+                    break
+            try:
+                searchParam=int(something[(len(search)+begin):(len(search)+begin+length)])/100
+                return searchParam
+            except:
+                return None
+        else:
+            return None
+
+    print("to the solver we go")
 
 
-        # try:
-        # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=threadCount,options=["maxSolutions 1"]))
+    # try:
+        # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=threadCount,options=["maxSolutions","1"]))
     # except:
         # print("maxSolutions didn't work'")
 
     # try:
-        # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=threadCount,options=["allowableGap 90"]))
+        # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=threadCount,options=["allowableGap","90"]))
     # except:
         # print("allowableGap didn't work'")
 
 
     # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4,fracGap=fractGap,presolve=preSolving))
     #Solve the problem using Gurobi
-    # NewOptim.solve(pulp.GUROBI())
-    #solver = "Gurobi" #for unit testing
-
-    # CPLex
     try:
-        NewOptim.solve(pulp.CPLEX_CMD(msg=2))
-    except Exception as ex:
-        print("CPlex error: ", ex)
+        NewOptim.solve(pulp.CPLEX_CMD())
+
+    except Exception:
+        print('in except')
+        traceback.print_exc(file=sys.stdout)
+        return
+        # print(traceback.print_stack())
+        # print(repr(traceback.format_stack()))        
+    #solver = "Gurobi" #for unit testing
 
     #Time stamp for optimization solve time
     # solve_end_seconds = dt.datetime.today().hour*60*60 + dt.datetime.today().minute*60 + dt.datetime.today().second
     # solve_seconds = solve_end_seconds - start_seconds
     # print("Time taken to solve optimization was:" + str(solve_seconds)) #for unit testing
 
-    # Debugging
-    print("#####################################################################")
-    print(LpStatus[NewOptim.status])
-    print("#####################################################################")
-    # Debugging
-    NegativeCount = 0
-    LowCount = 0
-    TrueCount = 0
-    OneCount = 0
-    for (i, Store) in enumerate(Stores):
-        for (j, Category) in enumerate(Categories):
-            for (k, Level) in enumerate(Levels):
-                if value(st[Store][Category][Level]) == 1:
-                    # print(st[Store][Category][Level]) #These values should only be a one or a zero
-                    OneCount += 1
-                elif value(st[Store][Category][Level]) > 0:
-                    # print(st[Store][Category][Level],"Value is: ",value(st[Store][Category][Level])) #These values should only be a one or a zero
-                    TrueCount += 1
-                elif value(st[Store][Category][Level]) == 0:
-                    # print(value(st[Store][Category][Level])) #These values should only be a one or a zero
-                    LowCount += 1
-                elif value(st[Store][Category][Level]) < 0:
-                    # print(st[Store][Category][Level],"Value is: ",value(st[Store][Category][Level])) #These values should only be a one or a zero
-                    NegativeCount += 1
-
-    ctNegativeCount = 0
-    ctLowCount = 0
-    ctTrueCount = 0
-    ctOneCount = 0
-
-    for (j, Category) in enumerate(Categories):
-        for (k, Level) in enumerate(Levels):
-            if value(ct[Category][Level]) == 1:
-                # print(value(ct[Store][Category][Level])) #These values should only be a one or a zero
-                ctOneCount += 1
-            elif value(ct[Category][Level]) > 0:
-                # print(ct[Store][Category][Level],"Value is: ",value(st[Store][Category][Level])) #These values should only be a one or a zero
-                ctTrueCount += 1
-            elif value(ct[Category][Level]) == 0:
-                # print(value(ct[Category][Level])) #These values should only be a one or a zero
-                ctLowCount += 1
-            elif value(ct[Category][Level]) < 0:
-                # print(ct[Category][Level],"Value is: ",value(st[Store][Category][Level])) #These values should only be a one or a zero
-                ctNegativeCount += 1
-
-    print("Status:", LpStatus[NewOptim.status])
-    print("---------------------------------------------------")
-    print("For Selected Tiers")
-    print("Number of Negatives Count is: ", NegativeCount)
-    print("Number of Zeroes Count is: ", LowCount)
-    print("Number Above 0 and Below 1 Count is: ", TrueCount)
-    print("Number of Selected Tiers: ", OneCount)
-    print("---------------------------------------------------")
-    print("For Created Tiers")
-    print("Number of Negatives Count is: ", ctNegativeCount)
-    print("Number of Zeroes Count is: ", ctLowCount)
-    print("Number Above 0 and Below 1 Count is: ", ctTrueCount)
-    print("Number of Created Tiers: ", ctOneCount)
-    print("Creating Outputs")
-
+    print('creating results')
     Results=pd.DataFrame(index=Stores,columns=Categories)
     for (i,Store) in enumerate(Stores):
         for (j,Category) in enumerate(Categories):
