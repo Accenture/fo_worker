cafd582843243cb8a0ccc4241f630e84946aa1dd
tkmahbz <peter.alfors@kohls.com>
2016-11-01 16:33:08 -0500

    added 'set' to CPLEX command to update mip gap

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -339,7 +339,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
 
     #Solve the problem using Gurobi
     try:
-        NewOptim.solve(pulp.CPLEX_CMD(msg=2, options=["mip tolerances mipgap .1"]))
+        NewOptim.solve(pulp.CPLEX_CMD(msg=2, options=["set mip tolerances mipgap .1"]))
         # NewOptim.solve(pulp.PULP_CBC_CMD(msg=2,threads=4))
     except Exception as ex:
         print('Solver failure: ', ex)
