5d41c3d68857733e628d8bbc695e312338e890c5
tkmahbz <peter.alfors@kohls.com>
2016-11-07 17:07:13 -0600

    Gurobi mipgap 0.01

--- src/FixtureOptimization/optimizer2.py
+++ src/FixtureOptimization/optimizer2.py
@@ -339,7 +339,7 @@ def optimize2(methodology,jobName,Stores,Categories,tierCounts,increment,weights
     # try:
         # NewOptim.solve(pulp.CPLEX_CMD(msg=2, options=["set mip tolerance mipgap " + str(fractGap),  "set threads " + str(threadCount)]))
     # NewOptim.solve(pulp.GUROBI(mip=True, msg=True, Threads=2))
-    NewOptim.solve(pulp.GUROBI(mip=True, msg=True, MIPgap=.5))
+    NewOptim.solve(pulp.GUROBI(mip=True, msg=True, MIPgap=.01))
     # NewOptim.solve(pulp.GUROBI_CMD())
     # NewOptim.solve(pulp.CPLEX_CMD(msg=2, options=["set mip tolerance mipgap .05"]))
     # NewOptim.solve(pulp.CPLEX_CMD(msg=2))
